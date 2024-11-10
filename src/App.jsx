7169e5a8e10f72ae25470930cd33d488b6c8fd30
liuyuzhen99 <liuyuzhenbmjt@163.com>
2024-11-10 19:14:35 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+
+
+
+function App() {
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>Hello there!</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
