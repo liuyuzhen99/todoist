8dc34b5a919f78a39387ec94c7d4f4d7a0d9deb3
liuyuzhen99 <liuyuzhenbmjt@163.com>
2024-11-12 07:47:06 +0800

    install firebase&node-sass

--- src/components/layouts/Siderbar.jsx
+++ src/components/layouts/Siderbar.jsx
@@ -1,15 +1,43 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar } from "react-icons/fa";
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from "react-icons/fa";
 
 export const Siderbar = () => {
-    return (
-        <div className="sidebar" data-testid="siderbar">
-            <ul className="sidebar_generic">
-                <li><span><FaInbox/></span><span>Inbox</span></li>
-                <li><span><FaRegCalendar/></span><span>Today</span></li>
-                <li><span><FaRegCalendarAlt/></span><span>Next 7 days</span></li>
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className="sidebar" data-testid="siderbar">
+      <ul className="sidebar_generic">
+        <li>
+          <span>
+            <FaInbox />
+          </span>
+          <span>Inbox</span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendar />
+          </span>
+          <span>Today</span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendarAlt />
+          </span>
+          <span>Next 7 days</span>
+        </li>
+      </ul>
+      <div className="siderbar__middle">
+        <span>
+          <FaChevronDown />
+        </span>
+        <h2>Projects</h2>
+      </div>
+      <ul className="sidebar__projects">Projects will be here</ul>
+      Add Project Component Here!!
+    </div>
+  );
+};
---
+++ src/firebase.jsx
@@ -0,0 +1,11 @@
+const firebaseConfig = firebase.initializeApp({
+  apikey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messageSenderId: "",
+  apiId: "",
+});
+
+export { firebaseConfig as firebase };
