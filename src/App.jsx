6b92ac9a57bc955df65b4403b0f5a8f8ddd76227
liuyuzhen99 <liuyuzhenbmjt@163.com>
2024-11-16 18:23:30 +0800

    scss

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { Content } from "./components/layouts/Content";
 import { Header } from "./components/layouts/Header";
+import "./App.scss";
 
 function App() {
   return (
